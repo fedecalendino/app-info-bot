446f0f4b2f75e2d3f4e8b0390716bad49916cc1b
Fede Calendino <fede@calendino.com>
2022-09-04 16:53:52 +0200

    Add slack notifications

--- app/jobs.py
+++ app/jobs.py
@@ -1,9 +1,17 @@
 import logging
+import os
+from time import time, sleep
+
+from slackhooks.blocks.section import Section
+from slackhooks.blocks.text import MarkdownText
+from slackhooks.client import Message
 
 from app.config import REDDIT_SUBREDDITS
 from . import bot
 
 logger = logging.getLogger(__name__)
+
+NAME = "check"
 
 
 def check():
@@ -13,3 +21,33 @@ def check():
 
     for subreddit in REDDIT_SUBREDDITS:
         result[subreddit] = bot.analyze_subreddit(subreddit)
+
+
+def run():
+    try:
+        start = time()
+        logger.info("Running job '%s'", NAME)
+        check()
+        end = time()
+
+        message = f"finished successfully after {round(end - start)} seconds"
+    except Exception as exc:
+        logger.error("Error running job '%s': %s", NAME, str(exc))
+        message = f"finished unsuccessfully: {exc}"
+        sleep(1)
+
+    if not os.getenv("SLACK_WEBHOOK_URL"):
+        return
+
+    Message(
+        text=f"{NAME} > {message}.",
+        blocks=[
+            Section(
+                text=MarkdownText(
+                    text=f"*{NAME.upper()}* > {message}.",
+                ),
+            ),
+        ],
+    ).send(
+        webhook_url=os.getenv("SLACK_WEBHOOK_URL"),
+    )
--- cronjob_check/__init__.py
+++ cronjob_check/__init__.py
@@ -4,4 +4,4 @@ from app import jobs
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    jobs.check()
+    jobs.run()
