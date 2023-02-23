3f37974de83b12a903dbecd85c1cb83e4245d53a
Fede Calendino <fede@calendino.com>
2023-02-23 13:18:53 +0100

    Fix integration with AppSliced

--- app/stores/app_store/appsliced.py
+++ app/stores/app_store/appsliced.py
@@ -1,16 +1,50 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
 
 URL = "https://appsliced.co/apps?search={app_id}"
 
+EMAIL = os.getenv("APPSLICED_EMAIL")
+PASSWORD = os.getenv("APPSLICED_PASSWORD")
+
 
 class AppSliced:
     def __init__(self, app_id: str):
-        url = URL.format(app_id=app_id)
-        response = requests.get(url)
+        with requests.Session() as session:
+            url = "https://appsliced.co/f/f-login.php"
 
-        soup = BeautifulSoup(response.content, features="html.parser")
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Origin": "https://appsliced.co",
+                "Pragma": "no-cache",
+                "Referer": "https://appsliced.co/login",
+            }
+
+            body = {
+                "email": EMAIL,
+                "password": PASSWORD,
+                "rememberme": "1",
+                "type": "1",
+            }
+
+            session.cookies.update(
+                {
+                    "loc": "US",
+                    "refurl": f"%252Fapps%253Fsearch%253D{app_id}",
+                    "cstatus": "1",
+                    "UISstate": "3939313032",
+                }
+            )
+
+            response = session.post(
+                url,
+                headers=headers,
+                data=body,
+                allow_redirects=True,
+            )
+
+            soup = BeautifulSoup(response.content, features="html.parser")
 
         div = soup.find("div", class_="10u title")
         a = div.find("a")
